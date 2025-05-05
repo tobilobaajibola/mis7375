df71af66a1d514888ff9477c4b4bd7adf35ef2ad
Tobiloba <ajibolatobiloba@gmail.com>
2025-05-05 04:50:01 -0500

    iframe

--- extracredit/assets/js/script.js
+++ extracredit/assets/js/script.js
@@ -535,3 +535,18 @@ document.addEventListener("DOMContentLoaded", () => {
       // Optionally show an error message in the UI
     });
 });
+
+    // sticky header
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
